34ecbc1b7c3595bab064c5bb01407dcd0289a16b
s24452 <s24452@pjwstk.edu.pl>
2023-03-15 14:11:32 +0100

    1 update

--- skrypt1.py
+++ skrypt1.py
@@ -1,9 +1,25 @@
+#1
 a = "Python 2023"
 b = "Python 2023"
 c = "Python 2023"
 
+#1a
 bool(a==b)
-#bool(a==c)
 bool(b==c)
 
+#1b
+print(type(a), hex(id(a)))
+print(type(b), hex(id(b)))
+print(type(c), hex(id(c)))
 
+c="Java 11"
+print('\n')
+
+#1a ponownie
+bool(a==b)
+bool(b==c)
+
+#1b ponownie
+print(type(a), hex(id(a)))
+print(type(b), hex(id(b)))
+print(type(c), hex(id(c)))
