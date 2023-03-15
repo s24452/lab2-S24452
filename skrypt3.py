a5f80ce4c63a335acc78980b13612c61c86f799e
s24452 <s24452@pjwstk.edu.pl>
2023-03-15 15:16:25 +0100

    update

---
+++ skrypt3.py
@@ -0,0 +1,122 @@
+pytanie1 = input("pytanie: Jak masz na imie i nazwisko? ")
+print("odpowiedz: ",pytanie1)
+pytanie2 = input("pytanie: Najczestszym sposobem spedzania wolnego czasu jest dla Ciebie ")
+if pytanie2=="a" or pytanie2=="A":
+    print("odpowiedz: ","ogladanie telewizji")
+elif pytanie2=="b" or pytanie2=="B":
+    print("odpowiedz: ", "czytanie ksiazek")
+elif pytanie2 == "c" or pytanie2 == "C":
+    print("odpowiedz: ", "sluchanie muzyki")
+elif pytanie2 == "d" or pytanie2 == "D":
+    print("odpowiedz: ", "spotkanie z rodzna/przyjaciolmi")
+else :
+    inna=input("innej jakiej? ")
+    print("odpowiedz: ",inna)
+
+
+
+pytanie8=input("pytanie: W jakich okolicznosciach czytasz ksiazki najczesciej? ")
+if pytanie8== "a" or pytanie8== "A":
+    print("odpowiedz: ","podczas podrozy")
+elif pytanie8== "b" or pytanie8== "B":
+    print("odpowiedz: ", "w czasie wolnym")
+elif pytanie8 == "c" or pytanie8 == "C":
+    print("odpowiedz: ", "podczas pracy/nauki")
+elif pytanie8 == "d" or pytanie8 == "D":
+    print("odpowiedz: ", "w ogole nie czytam")
+else :
+    inna=input("innej jakiej? ")
+    print("odpowiedz: ",inna)
+
+    pytanie8 = input("pytanie:Jezeli spedzasz czas wolny czytajac ksaizki, jaki jest glowny powod takiego wyboru  ? ")
+    if pytanie8 == "a" or pytanie8 == "A":
+        print("odpowiedz: ", "chec poszerzenia wiedzy")
+    elif pytanie8 == "b" or pytanie8 == "B":
+        print("odpowiedz: ", "czytanie mnie relaksuje")
+    elif pytanie8 == "c" or pytanie8 == "C":
+        print("odpowiedz: ", "fakt, ze czytanie jest modne")
+    elif pytanie8 == "d" or pytanie8 == "D":
+        print("odpowiedz: ", "czytanie to moje hobby")
+    elif pytanie8 == "e" or pytanie8 == "E":
+        print("odpowiedz: ", "koniecznosx nauki")
+    elif pytanie8 == "f" or pytanie8 == "F":
+        print("odpowiedz: ", "odczuwam presje")
+    else:
+        inna=input("innej jakiej? ")
+        print("odpowiedz: ", inna)
+
+
+pytanie8=input("pytanie:Po ksiazki w jakiej formie siegasz najczesciej ? ")
+if pytanie8== "a" or pytanie8== "A":
+    print("odpowiedz: ","papierowe(tradycyjne)")
+elif pytanie8== "b" or pytanie8== "B":
+    print("odpowiedz: ", "e-booki na komputerze")
+elif pytanie8 == "c" or pytanie8 == "C":
+    print("odpowiedz: ", "e-booki na telefonie")
+elif pytanie8 == "d" or pytanie8 == "D":
+    print("odpowiedz: ", "e-booki na czytniku")
+else :
+    inna=input("innej jakiej? ")
+    print("odpowiedz: ",inna)
+
+pytanie8=input("pytanie:Ile ksiazek srednio czytasz w roku ? ")
+if pytanie8== "a" or pytanie8== "A":
+    print("odpowiedz: ","0")
+elif pytanie8== "b" or pytanie8== "B":
+    print("odpowiedz: ", "zadnej w calosci, jedynie fragmenty")
+elif pytanie8 == "c" or pytanie8 == "C":
+    print("odpowiedz: ", "1")
+elif pytanie8 == "d" or pytanie8 == "D":
+    print("odpowiedz: ", "2 lub 3")
+elif pytanie8 == "e" or pytanie8 == "E":
+        print("odpowiedz: ", "4-10")
+elif pytanie8 == "f" or pytanie8 == "F":
+        print("odpowiedz: ", "powyzej 10")
+
+
+
+
+pytanie8 = input("pytanie:Jak czesto czytasz ksiazki ? ")
+if pytanie8 == "a" or pytanie8 == "A":
+        print("odpowiedz: ", "codziennie")
+elif pytanie8 == "b" or pytanie8 == "B":
+        print("odpowiedz: ", "raz w tygodniu")
+elif pytanie8 == "c" or pytanie8 == "C":
+        print("odpowiedz: ", "raz na miesiac")
+elif pytanie8 == "d" or pytanie8 == "D":
+        print("odpowiedz: ", "raz na kilka mieisecy")
+elif pytanie8 == "e" or pytanie8 == "E":
+        print("odpowiedz: ", "raz na pol roku")
+elif pytanie8 == "f" or pytanie8 == "F":
+        print("odpowiedz: ", "raz na rok")
+elif pytanie8 == "g" or pytanie8 == "G":
+        print("odpowiedz: ", "wcale")
+
+pytanie8 = input("pytanie:Po jakie gatunki ksiazek siegasz najczesciej ? ")
+if pytanie8 == "a" or pytanie8 == "A":
+        print("odpowiedz: ", "krymina;")
+elif pytanie8 == "b" or pytanie8 == "B":
+        print("odpowiedz: ", "horoor")
+elif pytanie8 == "c" or pytanie8 == "C":
+        print("odpowiedz: ", "fantastyka")
+elif pytanie8 == "d" or pytanie8 == "D":
+        print("odpowiedz: ", "science fiction")
+elif pytanie8 == "e" or pytanie8 == "E":
+        print("odpowiedz: ", "przygodowe")
+elif pytanie8 == "f" or pytanie8 == "F":
+        print("odpowiedz: ", "romanse")
+elif pytanie8 == "g" or pytanie8 == "G":
+        print("odpowiedz: ", "naukowe")
+elif pytanie8 == "h" or pytanie8 == "H":
+    print("odpowiedz: ", "biograficzne")
+elif pytanie8 == "i" or pytanie8 == "I":
+        print("odpowiedz: ", "podroznicze")
+elif pytanie8 == "j" or pytanie8 == "J":
+    print("odpowiedz: ", "naukowe")
+elif pytanie8 == "k" or pytanie8 == "K":
+        print("odpowiedz: ", "poezje")
+else:
+        inna=input("innej jakiej? ")
+        print("odpowiedz: ", inna)
+
+
